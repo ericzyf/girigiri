bcb380bc1e3d3e43baf9e4ec9bffbe948fbb72f2
Yifan Zhang <yfzhang1999@gmail.com>
2020-01-21 18:37:59 +0800

    app bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,59 +2,33 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
       dark
+      hide-on-scroll
+      color="pink lighten-2"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-toolbar-title class="font-weight-medium">ギリギリ</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>{{ mdiMagnify }}</v-icon>
       </v-btn>
+      <v-btn color="white" class="indigo--text">SIGN IN</v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import { mdiMagnify } from '@mdi/js'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    mdiMagnify
   }),
-};
+}
 </script>
