ec5cc7faf38c9fa0c4b21b842b91f08a5527482e
Yifan Zhang <yfzhang1999@gmail.com>
2020-01-24 01:40:34 +0800

    SignIn page

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -13,9 +13,72 @@
         height="500"
         outlined
       >
-        <Logo
-          class="pink--text text--lighten-2"
-        ></Logo>
+        <v-list>
+          <v-list-item class="mt-2">
+            <v-list-item-content class="text-center">
+              <Logo
+                class="pink--text text--lighten-2 headline"
+              ></Logo>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="mx-auto" style="width:90%">
+            <v-list-item-content>
+              <v-text-field
+                label="Username"
+                clearable
+              ></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="mx-auto" style="width:90%">
+            <v-list-item-content>
+              <v-text-field
+                label="Password"
+                counter
+              ></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-checkbox
+                label="Remember me"
+                color="pink"
+                class="ml-6"
+              ></v-checkbox>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-container>
+                <v-row>
+                  <v-col>
+                    <div class="ml-4 grey--text text--lighten-1 overline">No account?</div>
+                    <router-link
+                      to="/"
+                      active-class="no-text-decoration"
+                      exact-active-class="no-text-decoration"
+                    >
+                      <span class="ml-4 font-weight-medium">
+                        Create account
+                      </span>
+                    </router-link>
+                  </v-col>
+                  <v-col>
+                    <v-row justify="end">
+                      <v-btn
+                        color="pink lighten-2"
+                        depressed
+                        dark
+                        class="mr-8"
+                      >
+                        SIGN IN
+                      </v-btn>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
       </v-card>
     </v-row>
   </v-container>
@@ -35,5 +98,9 @@ export default {
 .gradient-bg {
   background: rgb(238,174,202);
   background: linear-gradient(180deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+}
+
+.no-text-decoration {
+  text-decoration: none;
 }
 </style>
