721cd032b652975653efccbfd7bda831ae9949c9
Yifan Zhang <yfzhang1999@gmail.com>
2020-01-24 19:53:20 +0800

    SignUp page

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,7 +1,99 @@
 <template>
-  <v-container fluid>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <p>This is the sign up page</p>
+  <v-container
+    fluid
+    style="height:100%"
+    class="gradient-bg"
+  >
+    <v-row
+      style="height:100%"
+    >
+      <v-card
+        class="ma-auto"
+        width="450"
+        height="500"
+        outlined
+      >
+        <v-list>
+          <v-list-item class="mt-2">
+            <v-list-item-content class="text-center">
+              <Logo
+                class="pink--text text--lighten-2 headline"
+              ></Logo>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="mx-auto" style="width:90%">
+            <v-list-item-content>
+              <v-text-field
+                label="Email"
+                clearable
+              ></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="mx-auto" style="width:90%">
+            <v-list-item-content>
+              <v-text-field
+                label="Username"
+                clearable
+              ></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="mx-auto pa-0" style="width:90%">
+            <v-list-item-content class="py-0">
+              <v-container class="py-0">
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      label="Password"
+                      :append-icon="mdiEye"
+                      counter
+                    ></v-text-field>
+                  </v-col>
+                  <v-col>
+                    <v-text-field
+                      label="Confirm Password"
+                      :append-icon="mdiEye"
+                      counter
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="mx-auto" style="width:90%">
+            <v-list-item-content>
+              <v-btn
+                color="pink lighten-2"
+                depressed
+                dark
+                class="mr-8"
+              >
+                SIGN UP
+              </v-btn>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
+
+<script>
+import Logo from '../components/Logo.vue'
+import { mdiEye } from '@mdi/js'
+
+export default {
+  components: {
+    Logo
+  },
+  data: () => ({
+    mdiEye
+  })
+}
+</script>
+
+<style>
+.gradient-bg {
+  background: rgb(238,174,202);
+  background: linear-gradient(180deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+}
+</style>
