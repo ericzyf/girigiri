9297c46c38418e9f90d6211bde17763c8310b1ab
Yifan Zhang <yfzhang1999@gmail.com>
2020-01-29 01:17:36 +0800

    install and use cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 const PORT = 8081
