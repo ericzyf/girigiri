76b58f081189098089190db5516760cb1178bc64
Yifan Zhang <yfzhang1999@gmail.com>
2020-02-09 01:41:16 +0800

    auto sign in with the basicAuthHeader stored in localStorage

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,44 +1,88 @@
 <template>
   <v-app>
-    <AppBar></AppBar>
-    <v-snackbar
-      bottom
-      v-model="gSnackbar.on"
-      :color="gSnackbar.color"
-      :timeout="gSnackbar.timeout"
-    >
-      {{ gSnackbar.text }}
-      <v-btn
-        text color="pink"
-        @click="gSnackbar.on = false"
+    <template v-if="appLoading">
+      <v-overlay :value="true">
+        <v-progress-circular
+          indeterminate
+          color="pink"
+          size="72"
+        ></v-progress-circular>
+      </v-overlay>
+    </template>
+    <template v-else>
+      <AppBar></AppBar>
+      <v-snackbar
+        bottom
+        v-model="gSnackbar.on"
+        :color="gSnackbar.color"
+        :timeout="gSnackbar.timeout"
       >
-        CLOSE
-      </v-btn>
-    </v-snackbar>
+        {{ gSnackbar.text }}
+        <v-btn
+          text color="pink"
+          @click="gSnackbar.on = false"
+        >
+          CLOSE
+        </v-btn>
+      </v-snackbar>
 
-    <v-content>
-      <router-view></router-view>
-    </v-content>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
 
-    <Footer></Footer>
+      <Footer></Footer>
+    </template>
   </v-app>
 </template>
 
 <script>
 import AppBar from './components/AppBar.vue'
 import Footer from './components/Footer.vue'
+import AuthService from './api/AuthService.js'
 
 export default {
   components: {
     AppBar,
     Footer
   },
+
   data: () => ({
+    appLoading: true
   }),
+
   computed: {
     gSnackbar() {
       return this.$store.state.gSnackbar
     }
+  },
+
+  async created() {
+    // check if there is basic auth header stored in localStorage
+    const h = localStorage.getItem('basicAuthHeader')
+    if (h !== null) {
+      // find previously stored basic auth header
+      // try to sign in with the header
+      const res = await AuthService.get({
+        'Authorization': h
+      })
+      if (res.auth) {
+        // sign in successfully
+        this.$store.commit('setLoginStatus', true)
+        this.$store.commit('setBasicAuthHeader', h)
+        this.$store.commit('setUserInfo', res.userInfo)
+        this.$store.commit('setGlobalSnackbar', {
+          on: true,
+          color: 'pink lighten-2',
+          timeout: 1500,
+          text: `Welcome, ${res.userInfo.username}!`
+        })
+      } else {
+        // failed to sign in
+        localStorage.removeItem('basicAuthHeader')
+      }
+    }
+    // finish loading
+    this.appLoading = false
   }
 }
 </script>
