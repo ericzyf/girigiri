0676f68f3fee3abc61db58e977d3ae432c286ad9
Yifan Zhang <yfzhang1999@gmail.com>
2020-02-09 02:28:38 +0800

    encode username in utf8

--- client/src/utils/BasicAuth.js
+++ client/src/utils/BasicAuth.js
@@ -1,1 +1,5 @@
-export default (name, pass) => 'Basic ' + window.btoa(`${name}:${pass}`)
+export default (name, pass) => {
+    const E = new TextEncoder()
+    name = String.fromCharCode(...E.encode(name))
+    return 'Basic ' + window.btoa(`${name}:${pass}`)
+}
