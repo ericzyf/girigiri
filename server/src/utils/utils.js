4e30e8bbb92c96c77b60ad55a036fc43735387ba
Yifan Zhang <yfzhang1999@gmail.com>
2020-02-10 17:10:23 +0800

    server side utils

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -1,8 +1,14 @@
 module.exports = {
+
   isEmptyObject(obj) {
     for (const p in obj) {
       return false
     }
     return true
+  },
+
+  isUndefined(obj) {
+    return typeof obj === 'undefined'
   }
+
 }
