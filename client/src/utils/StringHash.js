2e6304ad83828fd9d4e4d3702ec4b2c3550c86a1
Yifan Zhang <yfzhang1999@gmail.com>
2020-02-11 22:58:46 +0800

    sjcl sha256

--- client/src/utils/StringHash.js
+++ client/src/utils/StringHash.js
@@ -1,23 +1,25 @@
-/**
- * Calculate the SHA-256 digest of a UTF-8 encoded string
- * Based on the Web Cryptography API
- *
- * Reference:
- * https://www.w3.org/TR/WebCryptoAPI/
- * https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API
- */
+const sjcl = require('sjcl')
 
 const ENCODER = new TextEncoder()
 
 export default {
   async sha256(str) {
-    const res = await window.crypto.subtle.digest('SHA-256', ENCODER.encode(str))
-    const u8Array = new Uint8Array(res)
-    let hexBuf = new Array(32)
-    for (let i = 0; i < 32; ++i) {
-      let s = u8Array[i].toString(16)
-      hexBuf[i] = (s.length == 1) ? ('0' + s) : s
+    if (window.crypto.subtle) {
+      // use web crypto if available
+      console.log('sha256-webcrypto')
+      const res = await window.crypto.subtle.digest('SHA-256', ENCODER.encode(str))
+      const u8Array = new Uint8Array(res)
+      let hexBuf = new Array(32)
+      for (let i = 0; i < 32; ++i) {
+        let s = u8Array[i].toString(16)
+        hexBuf[i] = (s.length == 1) ? ('0' + s) : s
+      }
+      return hexBuf.join('')
+    } else {
+      // use sjcl
+      console.log('sha256-sjcl')
+      const b = sjcl.hash.sha256.hash(str)
+      return sjcl.codec.hex.fromBits(b)
     }
-    return hexBuf.join('')
   }
 }
